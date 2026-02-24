f1e2d28569fc510cdffe5512727b86beec0dd581
Avaco Sistemas <avacosistemas@gmail.com>
2026-02-24 11:07:20 -0300

    ajustes pantalla borrado grupo de usuario

--- src/app/resources/seguridad_grupo_usuario/grid/seguridad_grupo_usuario.grid.ts
+++ src/app/resources/seguridad_grupo_usuario/grid/seguridad_grupo_usuario.grid.ts
@@ -22,59 +22,56 @@ export const SEGURIDAD_GRUPO_USUARIO_GRID_DEF: GridDef = {
   ],
   sortAllColumns: true,
   displayedColumns: [
-    'id',
-    'idGrupo',
-    'idUsuario',
     'perfilNombre'
   ],
-  deleteAction: false,
-  actions: [
-    {
-      actionNameKey: 'seguridad_grupo_permiso_grid_def_delete',
-      icon: 'delete',
-      formDef: {
-        showSubmitContinue: false,
-        fields: [
-          {
-            key: 'idUsuario',
-            labelKey: 'seguridad_grupo_permiso_grid_def_column_idgrupo',
-            controlType: 'hidden',
-            mappingQuerystring: true
-          },
-          {
-            key: 'id',
-            labelKey: 'seguridad_grupo_permiso_grid_def_column_idpermiso',
-            label: 'idPermiso',
-            controlType: 'hidden'
-          },
-          {
-            key: 'ninguno',
-            label: 'Se borrará el siguiente grupo',
-            labelKey: 'Se borrará el siguiente grupo',
-            controlType: 'label',
-            value: 'Se borrará el siguiente grupo'
-          },
-          {
-            key: 'perfilNombre',
-            disabled: true,
-            labelKey: 'seguridad_grupo_permiso_grid_def_column_idpermiso',
-            label: 'Grupo',
-            controlType: 'textbox'
-          },
-          {
-            key: 'aceptar',
-            labelKey: 'Borrar',
-            label: 'Borrar',
-            controlType: 'checkbox',
-            value: false
-          },
-        ]
-      },
-      ws: {
-        key: 'contenido_grid_def_button_action_resumen',
-        url: PREFIX_DOMAIN_API + 'acceso',
-        method: 'PUT'
-      }
-    }
-  ]
+  deleteAction: true,
+  // actions: [
+  //   {
+  //     actionNameKey: 'seguridad_grupo_permiso_grid_def_delete',
+  //     icon: 'delete',
+  //     formDef: {
+  //       showSubmitContinue: false,
+  //       fields: [
+  //         {
+  //           key: 'idUsuario',
+  //           labelKey: 'seguridad_grupo_permiso_grid_def_column_idgrupo',
+  //           controlType: 'hidden',
+  //           mappingQuerystring: true
+  //         },
+  //         {
+  //           key: 'idGrupo',
+  //           labelKey: 'seguridad_grupo_permiso_grid_def_column_idpermiso',
+  //           label: 'idPermiso',
+  //           controlType: 'hidden'
+  //         },
+  //         {
+  //           key: 'ninguno',
+  //           label: 'Se borrará el siguiente grupo',
+  //           labelKey: 'Se borrará el siguiente grupo',
+  //           controlType: 'label',
+  //           value: 'Se borrará el siguiente grupo'
+  //         },
+  //         {
+  //           key: 'perfilNombre',
+  //           disabled: true,
+  //           labelKey: 'seguridad_grupo_permiso_grid_def_column_idpermiso',
+  //           label: 'Grupo',
+  //           controlType: 'textbox'
+  //         },
+  //         {
+  //           key: 'aceptar',
+  //           labelKey: 'Borrar',
+  //           label: 'Borrar',
+  //           controlType: 'checkbox',
+  //           value: false
+  //         },
+  //       ]
+  //     },
+  //     ws: {
+  //       key: 'contenido_grid_def_button_action_resumen',
+  //       url: PREFIX_DOMAIN_API + 'acceso',
+  //       method: 'PUT'
+  //     }
+    // }
+  // ]
 };
